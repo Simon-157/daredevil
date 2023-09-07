62cf6783a1897cdd8ddb59807f72808fec91c426
Simon-157 <boatengsimonjnr157@gmail.com>
2023-09-07 01:48:58 +0000

    feat:profile page and settings page added

--- src/pages/protected/profile/Profile.tsx
+++ src/pages/protected/profile/Profile.tsx
@@ -1,27 +1,31 @@
+import { useFirebaseAuth } from "../../../bass/auth/auth";
 import ProfileStyles from "./Profile.module.css";
 
 const Profile = () => {
+  const {user} = useFirebaseAuth()
+
+  console.log(user?.photoURL);
   
 
   return (
     <div className={ProfileStyles.profile}>
-      {/* <div className={ProfileStyles.profile_picture}>
-        <img src={auth?.profile_picture || defaultProfilePic} alt="" />
+      <div className={ProfileStyles.profile_picture}>
+        <img src={user?.photoURL || ""} alt="https://i.pravatar.cc/75" />
       </div>
       <div className={ProfileStyles.profile_details}>
         <section>
           <small>username</small>
-          <h2>{auth.name}</h2>
+          <h2>{user?.displayName}</h2>
         </section>
         <section>
           <small>email</small>
-          <h2>{auth.email}</h2>
+          <h2>{user?.email}</h2>
         </section>
         <section>
           <small>joined</small>
-          <h2>{auth.joined_at}</h2>
+          <h2>{user?.phoneNumber}</h2>
         </section>
-      </div> */}
+      </div>
     </div>
   );
 };
--- src/pages/protected/settings/Settings.tsx
+++ src/pages/protected/settings/Settings.tsx
@@ -1,5 +1,48 @@
+
+import { useFirebaseAuth } from "../../../bass/auth/auth";
+import Button from "../../../components/button/Button";
+import ProfileStyles from "../profile/Profile.module.css";
+
 const Settings = () => {
-  return <div>Settings</div>;
+  const {user} = useFirebaseAuth()
+
+  console.log(user?.photoURL);
+  
+  const btnStyles = {
+
+    backgroundColor: "#F498671A",
+    border:"1px solid rgba(244, 152, 103, 0.10)",
+    color:"#F49867"
+
+  }
+
+  return (
+    <div className={ProfileStyles.profile}>
+      <div className={ProfileStyles.profile_picture}>
+        <img src={user?.photoURL || ""} alt="https://i.pravatar.cc/75" />
+      </div>
+      <div className={ProfileStyles.profile_details}>
+        <section>
+          <small>username</small>
+          <h2>{user?.displayName}</h2>
+        </section>
+        <section>
+          <small>email</small>
+          <h2>{user?.email}</h2>
+        </section>
+        <section>
+          <small>joined</small>
+          <h2>01-02-2023</h2>
+        </section>
+        <div className={ProfileStyles.edit_btns}> 
+
+          <Button children={<h3>update</h3>} type={"solid"} />
+          <Button children={<h3>delete</h3>} type={""} style={btnStyles}/>
+        </div>
+      </div>
+
+    </div>
+  );
 };
 
 export default Settings;
